9755da70bf8516bde5d9a583a08d84f849bf383d
DusanNesic95 <dusannesic001@gmail.com>
2017-02-23 18:53:09 +0100

    Dodata opcija prikaza modala.

--- Umiya/www/js/HomePageController.js
+++ Umiya/www/js/HomePageController.js
@@ -1,5 +1,5 @@
 angular.module('starter')
-.controller("HomePageController", function($scope, $state) {
+.controller("HomePageController", function($scope, $state, $ionicModal) {
 	$scope.logout = function() {
 		$state.go('login');
 	}
@@ -26,4 +26,29 @@ angular.module('starter')
 			price: 17
 		}
 	]
+
+	$ionicModal.fromTemplateUrl('../modals/holdModal.html', {
+	scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+	$scope.openModal = function() {
+		$scope.modal.show();
+	};
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
+	// Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.modal.remove();
+	});
+	// Execute action on hide modal
+	$scope.$on('modal.hidden', function() {
+	// Execute action
+	});
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+	// Execute action
+	});
 });
