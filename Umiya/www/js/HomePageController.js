0e341afb5737e55bbb94a9b2c4eef45f918889ab
Milica Jovanovic <j.milica95@yahoo.com>
2017-02-24 00:58:41 +0100

    Kreiran modal payTM (bez slike za qr kod)

--- Umiya/www/js/HomePageController.js
+++ Umiya/www/js/HomePageController.js
@@ -101,4 +101,29 @@ angular.module('starter')
 	$scope.$on('modal.removed', function() {
 	// Execute action
 	});
+
+	$ionicModal.fromTemplateUrl('../modals/payTmModal.html', {
+	scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.payTmModal = modal;
+	});
+	$scope.openPayTmModal = function() {
+		$scope.payTmModal.show();
+	};
+	$scope.closePayTmModal = function() {
+		$scope.payTmModal.hide();
+	};
+	// Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.payTmModal.remove();
+	});
+	// Execute action on hide modal
+	$scope.$on('modal.hidden', function() {
+	// Execute action
+	});
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+	// Execute action
+	});
 });
