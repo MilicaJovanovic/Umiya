2f84baabcdea38e898782b79b127be9c07458c85
DusanNesic95 <dusannesic001@gmail.com>
2017-02-24 09:27:39 +0100

    Dodata fiktivna logika za login uz komentare sta treba promeniti.

--- Umiya/www/js/LoginController.js
+++ Umiya/www/js/LoginController.js
@@ -1,6 +1,19 @@
 angular.module('starter')
 .controller("LoginController", function($scope, $state) {
+	$scope.username = {
+		value : ""
+	}
+	$scope.password = {
+		value : ""
+	}
+
+	//TO DO, replace static login check with login logic
 	$scope.login = function() {
-		$state.go('amount');
+		if ($scope.username.value === 'admin' &&
+			$scope.password.value === 'admin') {
+			$state.go('amount');
+		} else {
+			alert("Username or password is not correct!");
+		}
 	}
 });
